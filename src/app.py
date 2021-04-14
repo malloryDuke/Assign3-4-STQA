55512743387a7788ab2e8046c8b44e0348659bb8
malloryDuke <mnd199@msstate.edu>
2021-04-14 15:59:13 -0500

    handled invalid inputs

--- src/app.py
+++ src/app.py
@@ -19,10 +19,15 @@ def getBMI():
     inches = str(inches)
     weight = str(weight)
 
-    bmi = bmiFunctions.calcBMI(int(feet), int(inches), int(weight))
-    category = bmiFunctions.getBMICategory(bmi)
-    formatBMI = "{:.4f}".format(bmi)
-    return render_template('main.html', bmi= "Your BMI is: " + str(formatBMI) + " [" + category + "]", feet=feet, inches=inches, weight=weight)
+    if ((int(feet) <= 0) or (int(inches) <= 0) or (int(weight) <= 0)):
+        finalBMI = "Feet, inches, or weight cannot be negative or equal to 0 -- Enter a valid feet, inches, or weight"
+    else:
+        bmi = bmiFunctions.calcBMI(int(feet), int(inches), int(weight))
+        category = bmiFunctions.getBMICategory(bmi)
+        formatBMI = "{:.4f}".format(bmi)
+        finalBMI = "Your BMI is: " + str(formatBMI) + " [" + category + "]"
+
+    return render_template('main.html', bmi=finalBMI, feet=feet, inches=inches, weight=weight)
 
 @app.route('/getRetAge', methods=['POST'])
 def getRetAge():
@@ -36,9 +41,20 @@ def getRetAge():
     perSaved = str(perSaved)
     goal = str(goal)
 
-    retAge = retFunctions.getRetirementAge(int(age), int(salary), float(perSaved), int(goal))
-    retCat = retFunctions.getRetirementCategory(retAge)
-    return render_template('main.html', retAge = "Your retirement age is: " + str(retAge) + " - " + retCat, currentAge = age, salary=salary, perSaved=perSaved, goal=goal)
+    if (int(age) <= 0):
+        retAge = "Current age cannot be negative or equal to 0 -- Enter a correct age"
+    elif (int(salary) <= 0):
+        retAge = "Salary cannot be negative or equal to 0 -- Enter a correct salary"
+    elif (float(perSaved) <= 0):
+        retAge = "Percent saved cannot be negative or 0 -- Enter a correct value"
+    elif (int(goal) <= 0):
+        retAge = "Savings goal cannot be negative or 0 -- Enter a valid savings goal"
+    else:
+        retAgeValue = retFunctions.getRetirementAge(int(age), int(salary), float(perSaved), int(goal))
+        retCat = retFunctions.getRetirementCategory(retAgeValue)
+        retAge = "Your retirment age is: " + str(retAgeValue) + " - " + retCat
+
+    return render_template('main.html', retAge = retAge, currentAge = age, salary=salary, perSaved=perSaved, goal=goal)
 
 #host='localhost', port='5050'
 app.run()
